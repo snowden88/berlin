0d90009cca3b9ada520d5fc4069f5d4ad8976e94
Michael <msnowd01@gmail.com>
2015-12-17 20:58:05 -0500

    added price

--- app/views/products/index.json.jbuilder
+++ app/views/products/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@products) do |product|
-  json.extract! product, :id, :name, :description, :image_url, :colour
+  json.extract! product, :id, :name, :description, :image_url, :colour, :price
   json.url product_url(product, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216234115) do
+ActiveRecord::Schema.define(version: 20151218012907) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151216234115) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.integer  "price"
   end
 
 end
