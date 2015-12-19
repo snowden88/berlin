09ab3d077f19ef1532ef83b562f37da97e3a01db
Michael <msnowd01@gmail.com>
2015-12-18 20:52:37 -0500

    deleted double resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :products
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  
+
   get 'static_pages/contact'
 
   get 'static_pages/about'
