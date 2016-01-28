a613cb6db6e8a4cf22e9e2f12e4a94625560d5c1
Michael <msnowd01@gmail.com>
2016-01-27 21:52:33 -0500

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/admin_login'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
