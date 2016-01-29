2d5003450b817b8ee3d128d00d32b56eb3392464
Michael <msnowd01@gmail.com>
2016-01-29 18:35:27 -0500

    changed actionmailer controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,13 +6,17 @@ class UserMailer < ApplicationMailer
     mail(:from => email,
         :to => 'your-email@example.com',
         :subject => "Berlin Bikes Response from: #{name}")
+
   end
 
   def thank_you
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
+    ActionMailer::Base.mail(:from => @email,
+        :to => 'your-email@example.com',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver_now
   end
 
 end
