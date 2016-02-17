9a715aacc49e89b0d5a6184a1de878f2d0575919
Michael <msnowd01@gmail.com>
2016-02-17 18:54:08 -0500

    added avg rating to model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -68,10 +68,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  def average_rating
-    comments.average(:rating).to_f
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
