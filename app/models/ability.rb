68fe2244cc3d92fe7e3a5f0549705074cd95a7db
Michael <msnowd01@gmail.com>
2016-02-18 19:29:04 -0500

    added cancan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :read, :all
+    end
   end
-
 end
