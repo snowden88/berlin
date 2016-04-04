cd815464d365f3916ba12316cb8f870d28e81b6b
Michael <msnowd01@gmail.com>
2016-04-04 18:50:27 -0400

    session expiry

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,6 +23,10 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+
+  #session expiry
+  config.timeout_in = 3.hours
+
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
