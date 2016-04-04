edb9a812c3ceedc7a2c6305e1a0a591b308cea5d
Michael <msnowd01@gmail.com>
2016-04-04 18:43:23 -0400

    added timeouts for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :timeoutable, :validatable
     has_many :orders
 
   after_create :send_welcome_email
