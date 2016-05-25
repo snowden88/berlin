6399f0398ee297236fd1644396c7f769e26f1c42
Michael <msnowd01@gmail.com>
2016-05-25 17:45:50 -0400

    redis

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ end
 gem 'stripe'
 #memcaching
 gem 'dalli'
+#redis
+gem 'redis-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.force_ssl = false
